5ac6628201da5ad31fe241bd812fc2b72eea04b3
Mohammad AL Mechkor <leaderiop@gmail.com>
2024-10-08 23:53:27 +0200

    refact(SideBar): update code structure

--- app/components/pagewrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-const pagewrapper = ( {children}:{children:ReactNode}) => {
-  return (
-    <div className='bg-slate-50 flex-grow text-black p-2 mt-16 pl-[20.4rem] '>pagewrapper</div>
-  )
-}
-
-export default pagewrapper
