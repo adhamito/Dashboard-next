04874bce67a2779920c047837ffb1576e4221c24
adhamito <aalmechkor2@gmail.com>
2024-10-08 12:48:56 +0100

    (feat) update sidebar by adding logo

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
  const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <aside className='fixed bg-[#31353d] text-gray-500 z-50 h-full shadow-gray-500/20 transition duration-300 ease-in-out w-[20rem]'>
+        <div className='flex items-center relative py-5 px-3.5 '>
+        <Image src="https://www.cdnlogo.com/logos/j/41/jira.svg" alt='logo' width={40} height={20} className='mx-3.5 min-h-fit'></Image>
+        <h3 className='pl-2 font-bold text-2xl text-[#e6e9ee] min-w-max'>
+            D-brain
+        </h3>
+        
+        </div>
+    </aside>
   )
 }
 export default SideBar
