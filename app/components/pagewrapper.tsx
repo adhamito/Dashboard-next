d8e07e5f59f49be8ccc9bad4aeab5eb1c5366da4
Mohammad Almechkor <leaderiop@gmail.com>
2024-10-08 22:24:40 +0200

    Revert "Components add"

--- app/components/pagewrapper.tsx
+++ app/components/pagewrapper.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 const pagewrapper = ( {children}:{children:ReactNode}) => {
   return (
-    <div className='bg-slate-50 flex-grow text-black p-2 mt-16 pl-[20.4rem] '>pagewrapper</div>
+    <div>pagewrapper</div>
   )
 }
 
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,38 +1,18 @@
-import React from 'react';
-import Image from 'next/image';
-import { SideBarItems } from '@/SideBar_constant';
-import SidebarItem from '../components/sidebar_item';
+import React from 'react'
+import Image from 'next/image'
 
-interface SideBarProps {
-    Toggle: boolean; 
+ const SideBar = () => {
+  return (
+    <aside className='fixed bg-[#31353d] text-gray-500 z-50 h-full shadow-gray-500/20 transition duration-300 ease-in-out w-[20rem]'>
+        <div className='flex items-center relative py-5 px-3.5 '>
+        <Image src="https://www.cdnlogo.com/logos/j/41/jira.svg" alt='logo' width={40} height={20} className='mx-3.5 min-h-fit'></Image>
+        <h3 className='pl-2 font-bold text-2xl text-[#e6e9ee] min-w-max'>
+            D-brain
+        </h3>
+        
+        </div>
+    </aside>
+  )
 }
+export default SideBar
 
-const SideBar: React.FC<SideBarProps> = ({ Toggle }) => {
-    return (
-        <aside className={`fixed bg-[#31353d] text-gray-500 z-50 h-full shadow-gray-500/20 transition duration-300 ease-in-out ${Toggle ? 'w-[20rem]' : 'w-[5rem]'}`}>
-            <div className='flex items-center relative py-5 px-3.5'>
-                <Image
-                    src="https://www.cdnlogo.com/logos/j/41/jira.svg"
-                    alt='logo'
-                    width={40}
-                    height={20}
-                    className='mx-3.5 min-h-fit'
-                />
-                {Toggle && (
-                    <h3 className='pl-2 font-bold text-2xl text-[#e6e9ee] min-w-max'>
-                        D-brain
-                    </h3>
-                )}
-            </div>
-            <nav>
-                <ul className='mt-6'>
-                    {SideBarItems.map((item, index) => (
-                        <SidebarItem key={index} item={item} Toggle={Toggle} />
-                    ))}
-                </ul>
-            </nav>
-        </aside>
-    );
-};
-
-export default SideBar;
