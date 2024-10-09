2ce2e1838a1bb9fa8f4a6c6dee3b23faa8815d55
adhamito <100098044+adhamito@users.noreply.github.com>
2024-10-09 20:09:47 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import DashboardPage from './components/dashboard/page';
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>
-        Dashboard
-      </h1>
+    <div className="flex flex-row flex-wrap justify-between items-center 
+     min-h-screen p-8 pb-20 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)] z-0">
+   
+      <DashboardPage />
+      
+    
     </div>
   );
 }
