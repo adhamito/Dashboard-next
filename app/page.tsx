bca96227e209a4120594d3a125a369d4e514f11f
adhamito <aalmechkor2@gmail.com>
2024-10-09 11:16:16 +0100

    feat(page)add the charts and make a style

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import Image from "next/image";
+import BarChartComponent from "./components/ui/ChartEmod";
+import {AreaChartComponent} from "./components/ui/AreaChart"; 
+import {DataTableDemo} from "./components/ui/TableData";
+import { NewChart } from "@/app/components/ui/NewChart";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-   <h1>
-    Dashboard
-   </h1>
+    <div className="flex flex-row flex-wrap justify-between items-center 
+     min-h-screen p-8 pb-20 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)] z-0">
+        <div className="flex flex-row justify-center z-0 gap-6">
+        <NewChart />
+        <DataTableDemo />
+        
+      </div>
+     <div className="flex flex-row justify-center z-0 gap-6 ">
+     <BarChartComponent  />
+     <AreaChartComponent />
+     </div>
+      
+      
+    
     </div>
   );
 }
